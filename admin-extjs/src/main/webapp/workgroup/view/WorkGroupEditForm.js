8c44749d21ee96b1cf3d907296b6e1b2ab318bfc
sunlf <hanbi918@163.com>
2015-10-31 00:44:42 +0800

    add workgroup

---
+++ admin-extjs/src/main/webapp/workgroup/view/WorkGroupEditForm.js
@@ -0,0 +1,60 @@
+/**
+ * 工作组编辑表单
+ *
+ * @author majian <br/>
+ *         date:2015-7-24
+ * @version 1.0.0
+ */
+Ext.define('kalix.admin.workgroup.view.WorkGroupEditForm', {
+    extend: 'Ext.FormPanel',
+    requires: [
+        'kalix.admin.workgroup.viewModel.WorkGroupViewModel',
+        'kalix.admin.workgroup.controller.WorkGroupFormController'
+    ],
+    alias: 'widget.workGroupEditForm',
+    viewModel: {
+        type: 'workGroupViewModel'
+    },
+    controller: 'workGroupFormController',
+    xtype: 'workGroupEditForm',
+    labelAlign: 'center',
+    labelWidth: 75,
+    autoWidth: true,
+    autoHeight: true,
+    bodyStyle: "padding:15px",
+    frame: true,
+    jsonSubmit: true,
+    method: "PUT",
+    defaultType: 'textfield',
+    buttonAlign: "center",
+    items: [
+        {xtype: 'hiddenfield', name: 'id'},
+        {
+            fieldLabel: '名称',
+            itemId: 'nameId',
+            name: 'name',
+            allowBlank: false,
+            blankText: '名称不能为空!',
+            beforeLabelTpl: [
+                '<span style="color:red;font-weight:bold" data-qtip="必填选项">*</span>'
+            ]
+        },
+        {
+            xtype: 'textarea',
+            fieldLabel: '备注',
+            itemId: 'remarkId',
+            name: 'remark',
+            beforeLabelTpl: [
+                '<span  >&nbsp;&nbsp;</span>'
+            ]
+        }
+    ],
+    buttons: [
+        {
+            text: '保存', glyph: 0xf0c7, type: 'submit', handler: 'onUpdate'
+        },
+        {
+            text: '重置', glyph: 0xf0e2, handler: 'onEditReset'
+        }
+    ]
+});
