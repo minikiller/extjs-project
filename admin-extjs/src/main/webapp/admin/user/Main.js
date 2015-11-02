7d553210de2a98a569c5a4ec87d2e112f5331e5b
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-11-02 13:45:55 +0800

    add usersearchform

--- admin-extjs/src/main/webapp/admin/user/Main.js
+++ admin-extjs/src/main/webapp/admin/user/Main.js
@@ -6,67 +6,76 @@
  * @version 1.0.0
  */
 Ext.define('kalix.admin.user.Main', {
-  extend : 'Ext.container.Container',
-  requires : [
-    'kalix.admin.user.store.UserStore',  //用户模型集合
-    'kalix.admin.user.view.UserGrid',
-    'kalix.admin.user.controller.UserController'
-  ],
-  controller: 'userController',
+    extend: 'Ext.container.Container',
+    requires: [
+        'kalix.admin.user.store.UserStore',  //用户模型集合
+        'kalix.admin.user.view.UserGrid',
+        //'kalix.admin.user.controller.UserController',
+        'kalix.admin.user.view.UserSearchForm'
+    ],
+    //controller: 'userController',
 
-  items : [{
-      xtype : 'form',
-      title : '用户查询',
-      bodyPadding : 10,
-      layout : 'column',
-      margin : 10,
-      
-      method : "POST",
-      url : 'login',
-      
-      items : [{
-          xtype : 'textfield',
-          fieldLabel : '登录名',
-          labelAlign : 'right',
-          labelWidth : 60,
-          width : 200,
-          name : 'loginName'
-        }, {
-          xtype : 'textfield',
-          fieldLabel : '姓名',
-          labelAlign : 'right',
-          labelWidth : 40,
-          width : 200,
-          name : 'name'
-        }, {
-          xtype : 'combobox',
-          fieldLabel : '状态',
-          labelAlign : 'right',
-          labelWidth : 40,
-          editable : false,
-          name : 'available',
-          value : '-1',
-          store : [
-            ['-1', '全部'],
-            ['1', '启用'],
-            ['0', '停用']
-          ]
-        }, {
-          xtype : 'button',
-          text : '查询',
-          margin: '0 0 0 10',
-          handler : 'onSearch',
-          glyph : 'xf002@FontAwesome',
+    items: [
+        //{
+        //    xtype: 'form',
+        //    title: '用户查询',
+        //    bodyPadding: 10,
+        //    layout: 'column',
+        //    margin: 10,
+        //
+        //    method: "POST",
+        //    url: 'login',
+        //
+        //    items: [{
+        //        xtype: 'textfield',
+        //        fieldLabel: '登录名',
+        //        labelAlign: 'right',
+        //        labelWidth: 60,
+        //        width: 200,
+        //        name: 'loginName'
+        //    }, {
+        //        xtype: 'textfield',
+        //        fieldLabel: '姓名',
+        //        labelAlign: 'right',
+        //        labelWidth: 40,
+        //        width: 200,
+        //        name: 'name'
+        //    }, {
+        //        xtype: 'combobox',
+        //        fieldLabel: '状态',
+        //        labelAlign: 'right',
+        //        labelWidth: 40,
+        //        editable: false,
+        //        name: 'available',
+        //        value: '-1',
+        //        store: [
+        //            ['-1', '全部'],
+        //            ['1', '启用'],
+        //            ['0', '停用']
+        //        ]
+        //    }, {
+        //        xtype: 'button',
+        //        text: '查询',
+        //        margin: '0 0 0 10',
+        //        handler: 'onSearch',
+        //        glyph: 'xf002@FontAwesome',
+        //    }
+        //    ]
+        //},
+
+
+        //{
+        //    xtype: 'userGridPanel',
+        //    id: 'userGridPanel',
+        //    title: '用户列表',
+        //    margin: 10,
+        //    store: {
+        //        type: 'userStore'
+        //    }
+        //}
+        //,
+        {
+            xtype:'userSearchForm',
         }
-      ]
-    }, {
-      xtype : 'userGridPanel',
-      id : 'userGridPanel',
-      title : '用户列表',
-      margin : 10,
-      store : {
-        type : 'userStore'
-      }
-    }
-  ]
+    ]
 });
---
+++ admin-extjs/src/main/webapp/admin/user/view/UserSearchForm.js
@@ -0,0 +1,56 @@
+/**
+ * Created by lenovo on 2015/11/2.
+ */
+
+Ext.define('kalix.admin.user.view.UserSearchForm', {
+    extends: 'Ext.form.Panel',
+    requires: [
+        'kalix.admin.user.controller.UserController'
+    ],
+    alias: 'widget.userSearchForm',
+    xtype: 'userSearchFormPanel',
+    controller: 'userController',
+    title: '用户查询',
+    bodyPadding: 10,
+    layout: 'column',
+    margin: 10,
+    method: "POST",
+    url: 'login',
+    autoShow:true,
+    items: [
+        {
+                xtype: 'textfield',
+                fieldLabel: '登录名',
+                labelAlign: 'right',
+                labelWidth: 60,
+                width: 200,
+                name: 'loginName'
+            }, {
+                xtype: 'textfield',
+                fieldLabel: '姓名',
+                labelAlign: 'right',
+                labelWidth: 40,
+                width: 200,
+                name: 'name'
+            }, {
+                xtype: 'combobox',
+                fieldLabel: '状态',
+                labelAlign: 'right',
+                labelWidth: 40,
+                editable: false,
+                name: 'available',
+                value: '-1',
+                store: [
+                    ['-1', '全部'],
+                    ['1', '启用'],
+                    ['0', '停用']
+                ]
+            }, {
+                xtype: 'button',
+                text: '查询',
+                margin: '0 0 0 10',
+                handler: 'onSearch',
+                glyph: 'xf002@FontAwesome',
+            }
+            ]
+});
