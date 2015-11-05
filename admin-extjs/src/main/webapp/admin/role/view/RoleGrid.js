2b0dd8bfdd855f016e79cd2c94141993aac6f0d1
sunlf <hanbi918@163.com>
2015-11-06 00:07:32 +0800

    Ext.Json格式化代码迁移到application.js

--- admin-extjs/src/main/webapp/admin/role/view/RoleGrid.js
+++ admin-extjs/src/main/webapp/admin/role/view/RoleGrid.js
@@ -19,7 +19,7 @@ Ext.define('kalix.admin.role.view.RoleGrid', {
     autoLoad: true,
     stripeRows: true,
     manageHeight: true,
-    selModel: {selType: 'checkboxmodel', mode: "SIMPLE"},
+    //selModel: {selType: 'checkboxmodel', mode: "SIMPLE"},
     //bind: {
     //    store: '{roleStore}'
     //},
@@ -30,25 +30,32 @@ Ext.define('kalix.admin.role.view.RoleGrid', {
     //    }
     //}],
     columns: [
-        {text: '编号', dataIndex: 'id'},
-        {text: '名称', dataIndex: 'name', width: 120},
-        {text: '创建人', dataIndex: 'createBy', width: 60},
         {
-            text: '创建日期', dataIndex: 'creationDate', width: 120, renderer: function (value) {
+            xtype: "rownumberer",
+            text: "行号",
+            width: 50,
+            align: 'center'
+        },
+        {text: '编号', dataIndex: 'id', hidden: true},
+        {text: '名称', dataIndex: 'name', flex: 1},
+        {text: '创建人', dataIndex: 'createBy', flex: 1},
+        {
+            text: '创建日期', dataIndex: 'creationDate', flex: 2, renderer: function (value) {
             var createDate = new Date(value);
             return createDate.format("yyyy-MM-dd hh:mm:ss");
         }
         },
-        {text: '更新人', dataIndex: 'updateBy', width: 60},
+        {text: '更新人', dataIndex: 'updateBy', flex: 1},
         {
-            text: '更新日期', dataIndex: 'updateDate', width: 120, renderer: function (value) {
+            text: '更新日期', dataIndex: 'updateDate', flex: 2,
+            renderer: function (value) {
             var updateDate = new Date(value);
             return updateDate.format("yyyy-MM-dd hh:mm:ss");
-          }
+        }
         },
         {
             header: '操作',
-            width: 75,
+            flex: 1,
             xtype: "actioncolumn",
             items: [{
                 icon: "admin/resources/images/pencil.png",
