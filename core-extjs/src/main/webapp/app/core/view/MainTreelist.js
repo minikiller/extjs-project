fe137049451ae1d307bbd410b36d08290c1940cd
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-11-12 22:55:46 +0800

    do some comment on the routeId solution.

--- core-extjs/src/main/webapp/app/core/view/MainTreelist.js
+++ core-extjs/src/main/webapp/app/core/view/MainTreelist.js
@@ -20,26 +20,28 @@ Ext.define('kalix.core.view.MainTreelist', {
         navigationTreeStore.on("load", function () {
             _.each(last, function (item) {
                 var destoryItem = self.getItem(item);
-
+                //if the module parent item has been destroy.
+                //the child menu item will auto release.
+                //this will happen in case that module is expanded.
                 if (destoryItem != null) {
                     destoryItem.destroy();
                 }
             });
 
             var hasFind=false;
-
+            //the auto select tree item logic.
             navigationTreeStore.each(function (recorder) {
                 var item = self.getItem(recorder);
 
                 if (navigationTreeStore.treeSelInfo.selected) {
-                    //var routeId=navigationTreeStore.treeSelInfo.level1+ '/' +
-                    //    navigationTreeStore.treeSelInfo.level2;
                     var childNodes=item.getNode().childNodes;
 
                     for(var idx=0;idx<childNodes.length;++idx)
                     {
                         var childItem=self.getItem(childNodes[idx]);
-
+                        //we don't care about the app level anchor.
+                        //we just match the menu level anchor.
+                        //so we can config the menu unit to any module.
                         if(childItem.getNode().getData().routeId.split('/')[1]==navigationTreeStore.treeSelInfo.level2){
                             item.expand();
                             self.setSelection(childItem.getNode());
