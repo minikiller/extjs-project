ee83ffc397362bed2fe1630d59ae9fcb3bb1931f
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-11-27 17:48:45 +0800

    attachment module and Some of InitActivator not  unregist resource.

--- attachment-extjs/src/main/webapp/attachment/view/AttachmentForm.js
+++ attachment-extjs/src/main/webapp/attachment/view/AttachmentForm.js
@@ -7,23 +7,8 @@ Ext.define('kalix.attachment.view.AttachmentForm', {
     alias: 'widget.attachmentForm',
     xtype: 'attachmentForm',
     frame: true,
+    timeout: 60,
     items: [
-        //{
-        //    xtype: 'textfield',
-        //    fieldLabel: '附件编号',
-        //    labelAlign: 'right',
-        //    labelWidth: 60,
-        //    width: 200,
-        //    name: 'id'
-        //},
-        //{
-        //    xtype: 'textfield',
-        //    fieldLabel: '附件版本',
-        //    labelAlign: 'right',
-        //    labelWidth: 60,
-        //    width: 200,
-        //    name: 'rev'
-        //},
         {
             xtype: 'filefield',
             fieldLabel: 'attachment',
