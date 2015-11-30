822854bdf385e27190235757bdcd97bebe6c735c
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-11-30 14:11:11 +0800

    add toArray method to Ext.JSON

---
+++ extjs-wrapper/src/main/webapp/resources/js/ext-json.js
@@ -0,0 +1,13 @@
+/**
+ * @auchor chenyanxu
+ */
+
+Ext.JSON.toArray = function (obj) {
+    var rtn = [];
+
+    for (var key in obj) {
+        rtn[rtn.length] = {key: key, value: obj[key]};
+    }
+
+    return rtn;
+}
