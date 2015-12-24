bbceeb7ce463694944186dbd55717753244627b5
chenyanxu-rexen <chenyanxu@rexen.com.cn>
2015-12-24 13:07:30 +0800

    shiro.ini with config apply: add ConfigShiro.cfg file to etc folder file content: #shiro.ini path #path=shiro/shiro-http.ini path=shiro/shiro-https.ini

--- core-extjs/src/main/java/cn/com/rexen/extjs/core/internal/InitActivator.java
+++ core-extjs/src/main/java/cn/com/rexen/extjs/core/internal/InitActivator.java
@@ -1,13 +1,8 @@
 package cn.com.rexen.extjs.core.internal;
 
 import cn.com.rexen.core.util.SystemUtil;
-import org.apache.camel.component.servlet.DefaultHttpRegistry;
-import org.apache.camel.component.servlet.HttpRegistry;
-import org.apache.log4j.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.http.HttpService;
 
 /**
  * Created by sunlf on 14-3-23.
@@ -15,39 +10,26 @@ import org.osgi.service.http.HttpService;
 public class InitActivator implements BundleActivator {
 
     private static final String BUNDLE_NAME = " Extjs Core Web ";
-    private static BundleContext context;
-    private static Logger logger = Logger.getLogger(InitActivator.class);
-    private ServiceReference reference;
-    private HttpService httpService;
+    //private static BundleContext context;
+    //private static Logger logger = Logger.getLogger(InitActivator.class);
+    //private ServiceReference reference;
+    //private HttpService httpService;
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         SystemUtil.succeedPrintln(String.format("Start-up %s bundle!!", BUNDLE_NAME));
-        context = bundleContext;
-        reference = bundleContext.getServiceReference(HttpService.class.getName());
-        //httpService = (HttpService) bundleContext.getService(reference);
-//        WebContainer webContainer;
-//        webContainer.unregisterConstraintMapping();
-//        httpService.registerResources("/kalix/view", "/view", null);
+        //context = bundleContext;
+        //reference = bundleContext.getServiceReference(HttpService.class.getName());
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         SystemUtil.succeedPrintln(String.format("Stop %s bundle!!", BUNDLE_NAME));
-        /*if(httpService!=null){
-            httpService.unregister("CamelServlet");
-        }*/
-        HttpRegistry httpRegistry = DefaultHttpRegistry.getHttpRegistry("CamelServlet");
-        DefaultHttpRegistry.removeHttpRegistry("CamelServlet");
-        if (httpRegistry != null) {
-            httpRegistry.unregister(httpRegistry.getCamelServlet("CamelServlet"));
-            //httpRegistry = null;
-        }
 
-        if (reference != null){
-            bundleContext.ungetService(reference);
-        }
+        //if (reference != null){
+        //    bundleContext.ungetService(reference);
+        //}
 
-        context = null;
+        //context = null;
     }
 }
--- core-extjs/src/main/webapp/login.jsp
+++ core-extjs/src/main/webapp/login.jsp
@@ -1,10 +1,4 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%--<%--%>
-    <%--Subject subject=SecurityUtils.getSubject();--%>
-    <%--Session shiro = subject.getSession();--%>
-    <%--if(shiro.getAttribute("currentUsername")!=null)--%>
-        <%--response.sendRedirect("index.jsp");--%>
-<%--%>--%>
 <!DOCTYPE HTML>
 <html manifest="">
 <head>
@@ -20,16 +14,7 @@
     <script type="text/javascript" src="resources/js/login.js"></script> 
     <script type="text/javascript" src="resources/js/Config.js"></script>
     <script type="text/javascript" src="resources/js/DateFormat.js"></script>
-    <!-- 引入Font Awesome的css文件 -->
-   <%-- <link type="text/css" rel="stylesheet" href="resources/css/font-awesome.min.css"/>
-    <link type="text/css" rel="stylesheet" href="resources/css/index.css"/>--%>
 </head>
 <body>
-
-
-<%--<%response.setHeader("Refresh","5;URL=index.jsp");%>--%>
-
-
-
 </body>
 </html>
