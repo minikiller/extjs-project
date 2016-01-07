1ff8fb2f3ac7f2b8054e6a5abead58312a636842
chenyanxu <yanxu2016>
2016-01-07 08:40:36 +0800

    add sealApply module

--- demo-extjs/src/main/webapp/demo/sealapply/controller/SealApplyGridController.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author chenyanxu
- */
-Ext.define('kalix.demo.sealApply.controller.SealApplyGridController', {
-    extend: 'kalix.controller.BaseGridController',
-    alias: 'controller.sealApplyGridController'
-});
