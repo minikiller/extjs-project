0b859ab63275af64c575122a920dd58b9eb797b5
chenyanxu <yanxu2016>
2016-03-18 15:49:25 +0800

    fix the bug when click the datefield in the form and doing nothing,model.dirty=true

--- extjs-wrapper/src/main/webapp/app/model/BaseModel.js
+++ extjs-wrapper/src/main/webapp/app/model/BaseModel.js
@@ -10,6 +10,20 @@ Ext.define('kalix.model.BaseModel', {
 
         if (arguments.length == 0) {
             this.set('id', 0);
+
+            var dateFields = this.fields.filter(function (item) {
+                return item.type == 'date';
+            });
+
+            for (var fIndex = 0; fIndex < dateFields.length; ++fIndex) {
+                var tempDate = this.get(dateFields[fIndex].name);
+
+                if (tempDate) {
+                    tempDate.setSeconds(0);
+                    tempDate.setMilliseconds(0);
+                }
+            }
+
             this.modified = {};
             this.dirty = false;
         }
