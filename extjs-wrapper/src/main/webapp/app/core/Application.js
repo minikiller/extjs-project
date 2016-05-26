d8fd11206bf5ab09427de8fbe0c41afc1018280e
zangyanming <zyanming2002@163.com>
2016-05-26 16:11:51 +0800

    重写Ext.JSON.encodeDate函数时，时间格式改为24小时制。

--- extjs-wrapper/src/main/webapp/app/core/Application.js
+++ extjs-wrapper/src/main/webapp/app/core/Application.js
@@ -18,7 +18,7 @@ Ext.define('kalix.core.Application', {
     launch: function () {
         //格式化时间类型代码，防止出现T字符
         Ext.JSON.encodeDate = function (d) {
-            return Ext.Date.format(d, '"Y-m-d h:i:s"');
+            return Ext.Date.format(d, '"Y-m-d H:i:s"');
         };
         //Ext.ux.ActivityMonitor.init({ verbose : true });
         // Ext.ux.ActivityMonitor.start();
